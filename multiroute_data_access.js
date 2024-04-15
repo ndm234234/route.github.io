c862aa06cc335a2848112d7582d286fc071629b2
ndm234234 <110901871+ndm234234@users.noreply.github.com>
2024-04-15 13:06:37 +0300

    Add files via upload

---
+++ multiroute_data_access.js
@@ -0,0 +1,79 @@
+function init () {
+    // Создаем модель мультимаршрута.
+    var multiRouteModel = new ymaps.multiRouter.MultiRouteModel([
+            [55.841025, 37.387779],
+            [55.865239, 37.427834]
+        ], {
+            // Путевые точки можно перетаскивать.
+            // Маршрут при этом будет перестраиваться.
+            wayPointDraggable: true,
+            boundsAutoApply: true
+        }),
+
+        // Создаём выпадающий список для выбора типа маршрута.
+        routeTypeSelector = new ymaps.control.ListBox({
+            data: {
+                content: 'Как добраться'
+            },
+            items: [
+                new ymaps.control.ListBoxItem({data: {content: "Авто"},state: {selected: true}}),
+                new ymaps.control.ListBoxItem({data: {content: "Общественным транспортом"}}),
+                new ymaps.control.ListBoxItem({data: {content: "Пешком"}})
+            ],
+            options: {
+                itemSelectOnClick: false
+            }
+        }),
+        // Получаем прямые ссылки на пункты списка.
+        autoRouteItem = routeTypeSelector.get(0),
+        masstransitRouteItem = routeTypeSelector.get(1),
+        pedestrianRouteItem = routeTypeSelector.get(2);
+
+    // Подписываемся на события нажатия на пункты выпадающего списка.
+    autoRouteItem.events.add('click', function (e) { changeRoutingMode('auto', e.get('target')); });
+    masstransitRouteItem.events.add('click', function (e) { changeRoutingMode('masstransit', e.get('target')); });
+    pedestrianRouteItem.events.add('click', function (e) { changeRoutingMode('pedestrian', e.get('target')); });
+
+    ymaps.modules.require([
+        'MultiRouteCustomView'
+    ], function (MultiRouteCustomView) {
+        // Создаем экземпляр текстового отображения модели мультимаршрута.
+        // см. файл custom_view.js
+        new MultiRouteCustomView(multiRouteModel);
+    });
+
+    // Создаем карту с добавленной на нее кнопкой.
+    var myMap = new ymaps.Map('map', {
+            center: [55.750625, 37.626],
+            zoom: 7,
+            controls: [routeTypeSelector]
+        }, {
+            buttonMaxWidth: 300
+        }),
+
+        // Создаем на основе существующей модели мультимаршрут.
+        multiRoute = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
+            // Путевые точки можно перетаскивать.
+            // Маршрут при этом будет перестраиваться.
+            wayPointDraggable: true,
+            boundsAutoApply: true
+        });
+
+    // Добавляем мультимаршрут на карту.
+    myMap.geoObjects.add(multiRoute);
+
+    function changeRoutingMode(routingMode, targetItem) {
+        multiRouteModel.setParams({ routingMode: routingMode }, true);
+
+        // Отменяем выбор элементов.
+        autoRouteItem.deselect();
+        masstransitRouteItem.deselect();
+        pedestrianRouteItem.deselect();
+
+        // Выбираем элемент и закрываем список.
+        targetItem.select();
+        routeTypeSelector.collapse();
+    }
+}
+
+ymaps.ready(init);
